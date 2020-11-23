a313a4c6f0f477f17e160344d0ec196e9b72d5e9
GEEZEEBE <geezeebe@gmail.com>
2020-11-23 12:04:30 +0900

    descending speed down

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv) {
         }
         else {
             descend(c);
-            sleep_milli(300);
+            sleep_milli(500);
             // sleep_milli(20);
         }
 
